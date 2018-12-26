150b7366aa705ad68a05dfd736f05f995000965d
Sergey Lapin <sergey.lapin@cogentembedded.com>
2018-12-26 23:13:33 +0300

    Simplified code a bit

--- building_generator/outside_walls/zones/states/shrink_uniform.gd
+++ building_generator/outside_walls/zones/states/shrink_uniform.gd
@@ -1,4 +1,4 @@
-extends Node
+extends ZoneState
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -16,22 +16,22 @@ var state = CHECK
 func run(obj):
 	var tris = obj.tris
 	if state == CHECK:
-		var ok = true
-		for h in obj.outside_walls_segments:
-			if obj.collide(h, obj.outside_walls_xform):
-				ok = false
-			else:
-				for z in get_tree().get_nodes_in_group("zones"):
-					if z.shape == obj.shape:
-						continue
-					if !obj.collide(z.circle_shape, z.global_transform):
-						continue
-					else:
-						for k in z.convex_shapes:
-							if obj.collide(k, z.global_transform):
-								ok = false
-								break
-					
+		var ok = !check_shape_collision(obj)
+#		for h in obj.outside_walls_segments:
+#			if obj.collide(h, obj.outside_walls_xform):
+#				ok = false
+#			else:
+#				for z in get_tree().get_nodes_in_group("zones"):
+#					if z.shape == obj.shape:
+#						continue
+#					if !obj.collide(z.circle_shape, z.global_transform):
+#						continue
+#					else:
+#						for k in z.convex_shapes:
+#							if obj.collide(k, z.global_transform):
+#								ok = false
+#								break
+#					
 		if ok:
 			state = COMPLETE
 		else:
--- building_generator/zones/states/grow_uniform.gd
+++ building_generator/zones/states/grow_uniform.gd
@@ -1,10 +1,12 @@
-extends ZoneState
+extends Node
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
+enum {GROW, CHECK}
+var state = CHECK
 func _ready():
 	pass # Replace with function body.
 
@@ -12,6 +14,13 @@ func _ready():
 #func _process(delta):
 #	pass
 func run(obj):
-	return "next"
-func init(obj):
-	pass
+	match(state):
+		CHECK:
+			if !obj.room_is_valid() || obj.is_anything_collides_me():
+				return "grow_uniform"
+			else:
+				state = GROW
+		GROW:
+			if obj.rnd.randf() <= obj.grow_probability:
+				obj.grow_uniform()
+			state = CHECK
